c89b3e366bc49e48d5aae56f21b7a7b9ff170d55
GianniMS <1008999@hr.nl>
2024-01-16 21:01:30 +0100

    added API data for one location (amsterdam)

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { changeEvent, useState, useEffect} from "react";
+import axios from "axios";
+
 import "./style/card.css";
 import search_icon from "../components/assets/search.png";
 import wind_icon from "../components/assets/wind.png";
@@ -10,47 +12,85 @@ import rain_icon from "../components/assets/rain.png";
 import snow_icon from "../components/assets/snow.png";
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            weatherData: null,
+            loading: true,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.fetchWeatherData("Amsterdam"); // You can set the default city here
+    }
+
+    fetchWeatherData = async (city) => {
+        try {
+            const apiKey = '854834913f70db7c4bf96528aab15ab7';
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+            const response = await axios.get(apiUrl);
+            const temperatureInCelsius = response.data.main.temp - 273.15;
+
+            this.setState({
+                weatherData: {
+                    ...response.data,
+                    main: {
+                        ...response.data.main,
+                        temp: temperatureInCelsius,
+                    },
+                },
+                loading: false,
+            });
+        } catch (error) {
+            this.setState({
+                error: error.message,
+                loading: false,
+            });
+        }
+    };
 
     render() {
+        const {weatherData, loading, error,} = this.state;
+
         return (
             <div className="card-main">
-                <div className="search-bar">
-                    <input type="text" className="locationInput" placeholder="Select your city"/>
-                    <div className="si-container">
-                        <img src={search_icon} alt="" className="search-icon"/>
-                    </div>
-                </div>
-                <div className="weather-all">
-                    <div className="weather-main">
-                        <div className="main-icon">
-                            <img src={cloud_icon} alt="" className="m-icon"/>
-                        </div>
-                        <div className="main-info">
-                            20 Degrees
-                        </div>
-                        <div className="main-location">
-                            Amsterdam
-                        </div>
-                    </div>
-                    <div className="weather-details">
-                        <div className="detail">
-                            <div className="detail-icon">
-                                <img src={wind_icon} alt="" className="d-icon"/>
+                {loading && <p>Loading...</p>}
+                {error && <p>Error: {error}</p>}
+                {weatherData && (
+                    <div className="weather-all">
+                        <div className="weather-main">
+                            <div className="main-icon">
+                                <img src={cloud_icon} alt="" className="m-icon"/>
                             </div>
-                            <div className="detail-info">
-                                10Km/h <br/> Wind Speed
+                            <div className="main-info">
+                                {Math.round(weatherData.main.temp)}°C
+                            </div>
+                            <div className="main-location">
+                                Amsterdam
                             </div>
                         </div>
-                        <div className="detail">
-                            <div className="detail-icon">
-                                <img src={humidity_icon} alt="" className="d-icon"/>
+                        <div className="weather-details">
+                            <div className="detail">
+                                <div className="detail-icon">
+                                    <img src={wind_icon} alt="" className="d-icon"/>
+                                </div>
+                                <div className="detail-info">
+                                    {weatherData.wind.speed} Km/H <br/> Wind Speed
+                                </div>
                             </div>
-                            <div className="detail-info">
-                                20% <br/>Humidity
+                            <div className="detail">
+                                <div className="detail-icon">
+                                    <img src={humidity_icon} alt="" className="d-icon"/>
+                                </div>
+                                <div className="detail-info">
+                                    {weatherData.main.humidity}% <br/>Humidity
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         );
     }
